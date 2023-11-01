824127798c15310722c029c5e7b1b8f4da0ba4c5
egpivo <egpivo@gmail.com>
2023-11-01 15:21:53 +0800

    CHG: modify the attention method as the additive method

--- examples/translation/seq2seq.py
+++ examples/translation/seq2seq.py
@@ -71,12 +71,30 @@ class DecoderRNN(nn.Module):
         return logits, hidden
 
 
+class BahadanauAttention(nn.Module):
+    def __init__(self, hidden_size: int) -> None:
+        super().__init__()
+
+        self.w = nn.Linear(hidden_size, hidden_size)
+        self.u = nn.Linear(hidden_size, hidden_size)
+        self.v = nn.Linear(hidden_size, 1)
+
+    def forward(self, query: torch.tensor, keys: torch.tensor) -> tuple[torch.tensor]:
+        scores = self.v(torch.tanh(self.w(query) + self.u(keys)))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        weights = F.softmax(scores, dim=-1)
+        context = torch.bmm(weights, keys)
+        return context, weights
+
+
 class AttentionDecoderRNN(nn.Module):
     def __init__(
         self,
         hidden_size: int,
         output_size: int,
         dropout_rate: float = 0.1,
+        device: str = "cpu",
         max_length: int = MAX_LENGTH,
     ) -> None:
         super().__init__()
@@ -87,32 +105,52 @@ class AttentionDecoderRNN(nn.Module):
         self.max_length = max_length
 
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attention = nn.Linear(self.hidden_size * 2, self.max_length)
-        self.attention_combined = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.attention = BahadanauAttention(self.hidden_size)
 
         self.dropout = nn.Dropout(self.dropout_rate)
         self.gru = nn.GRU(self.hidden_size, self.hidden_size)
         self.output_layer = nn.Linear(self.hidden_size, self.output_size)
+        self.device = device
 
     def forward(
+        self,
+        encoder_outputs: torch.Tensor,
+        encoder_hidden: torch.Tensor,
+        target_tensor: torch.Tensor = None,
+    ) -> tuple[torch.Tensor]:
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=self.device)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+        attentions = []
+
+        for i in range(self.max_length):
+            decoder_output, decoder_hidden, attention_weights = self.forward_step(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_outputs.append(decoder_output)
+            attentions.append(attention_weights)
+
+            if target_tensor:
+                decoder_input = target_tensor[:, i].unsqueeze(1)
+            else:
+                _, top_i = decoder_output.topk(1)
+                decoder_input = top_i.squeeze(-1).detach()
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_probs = F.log_softmax(decoder_outputs, dim=-1)
+        attentions = torch.cat(attentions, dim=1)
+        return decoder_probs, decoder_hidden, attentions
+
+    def forward_step(
         self, input: torch.Tensor, hidden: torch.Tensor, encoder_outputs: torch.Tensor
     ) -> tuple[torch.Tensor]:
+        embedding = self.dropout(self.embedding(input))
+        query = hidden.permute(1, 0, 2)
 
-        embedding = self.embedding(input).view(1, 1, -1)
-        dropout_embedding = self.dropout(embedding)
+        context, attention_weights = self.attention(query, encoder_outputs)
+        input_gru = torch.cat((embedding, context), dim=2)
 
-        attention_weights = F.softmax(
-            self.attention(torch.cat((dropout_embedding[0], hidden[0]), 1), dim=1)
-        )
-        attention_applied = torch.bmm(
-            attention_weights.unsqueeze(0), encoder_outputs.unsqueeze(0)
-        )
-        concat_attention = torch.cat((embedding[0], attention_applied[0]), 1)
-        combined_attention = self.attention_combined(concat_attention).unsqueeze(0)
-
-        output, hidden = self.gru(F.relu(combined_attention), hidden)
-        log_prob = F.log_softmax(self.output_layer(output[0]), dim=1)
-        return log_prob, hidden, attention_weights
-
-    def initialize_hidden(self) -> torch.Tensor:
-        return torch.zeros(1, 1, self.hidden_size, device=self.device)
+        output, hidden = self.gru(input_gru, hidden)
+        logits = self.output_layer(output)
+        return logits, hidden, attention_weights

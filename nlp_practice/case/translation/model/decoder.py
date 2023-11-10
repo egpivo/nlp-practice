0432737c67a5a4fde1a4948fc1b691be9c58a1d8
egpivo <egpivo@gmail.com>
2023-11-10 16:22:13 +0800

    CHG: modify the class parameter; add evaluation notebook
    Signed-off-by: egpivo <egpivo@gmail.com>

--- nlp_practice/case/translation/model/decoder.py
+++ nlp_practice/case/translation/model/decoder.py
@@ -13,12 +13,14 @@ class Decoder(nn.Module):
         super().__init__()
 
         self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
-        self.output_layer = nn.Linear(hidden_size, output_size)
+        self.output_size = output_size
         self.dropout_rate = dropout_rate
-        self.dropout = nn.Dropout(self.dropout_rate)
         self.device = device
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size, batch_first=True)
+        self.output_layer = nn.Linear(self.hidden_size, self.output_size)
+        self.dropout = nn.Dropout(self.dropout_rate)
 
     def forward(
         self,

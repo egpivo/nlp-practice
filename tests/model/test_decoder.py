275e64c37d72877fec2414a81db0a9aca413d718
egpivo <egpivo@gmail.com>
2023-11-13 15:14:49 +0800

    FIX: fix the random seed to avoid the randomness
    Signed-off-by: egpivo <egpivo@gmail.com>

--- tests/model/test_decoder.py
+++ tests/model/test_decoder.py
@@ -11,7 +11,16 @@ def decoder_rnn():
     return DecoderRNN(hidden_size=64, output_size=10, dropout_rate=0.1, device="cpu")
 
 
+@pytest.fixture
+def attention_decoder_rnn():
+    torch.manual_seed(42)
+    return AttentionDecoderRNN(
+        hidden_size=64, output_size=10, dropout_rate=0.1, device="cpu"
+    )
+
+
 def test_decoder_rnn_forward(decoder_rnn):
+    torch.manual_seed(42)
     batch_size = 5
     max_length = MAX_LENGTH
     target_tensor = torch.randint(0, 10, (batch_size, max_length))
@@ -23,8 +32,8 @@ def test_decoder_rnn_forward(decoder_rnn):
     expected_shape = (batch_size, max_length, 10)
     assert decoder_probs.shape == expected_shape
 
-    expected_sum_value = -1160.2513
-    expected_mean_value = -2.3205
+    expected_sum_value = -1156.362548828125
+    expected_mean_value = -2.312725067138672
 
     torch.testing.assert_close(
         torch.sum(decoder_probs).item(), expected_sum_value, rtol=1e-5, atol=1e-5
@@ -34,18 +43,8 @@ def test_decoder_rnn_forward(decoder_rnn):
     )
 
 
-# Add more tests as needed for other methods and cases
-
-
-@pytest.fixture
-def attention_decoder_rnn():
-    torch.manual_seed(42)
-    return AttentionDecoderRNN(
-        hidden_size=64, output_size=10, dropout_rate=0.1, device="cpu"
-    )
-
-
 def test_attention_decoder_rnn_forward(attention_decoder_rnn):
+    torch.manual_seed(42)
     batch_size = 5
     hidden_size = 64
     output_size = 10
@@ -61,8 +60,8 @@ def test_attention_decoder_rnn_forward(attention_decoder_rnn):
     expected_shape = (batch_size, max_length, output_size)
     assert decoder_probs.shape == expected_shape
 
-    expected_sum_value = -1162.6511
-    expected_mean_value = -2.3298
+    expected_sum_value = -1163.7960205078125
+    expected_mean_value = -2.327592134475708
 
     torch.testing.assert_close(
         torch.sum(decoder_probs).item(), expected_sum_value, rtol=1e-5, atol=1e-5
